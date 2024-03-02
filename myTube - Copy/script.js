429a85cc67be07f6df784c0533c0b0e72c9f6794
aryan-negi-93 <134124253+aryan-negi-93@users.noreply.github.com>
2024-03-02 23:07:59 +0530

    Add files via upload

---
+++ myTube - Copy/script.js
@@ -0,0 +1,20 @@
+var logo = document.querySelector(".logo");
+var sidebar = document.querySelector(".side-bar");
+var container = document.querySelector(".container");
+
+logo.onclick = function(){
+     sidebar.classList.toggle("small-sidebar");
+     container.classList.toggle("big-container")
+}
+
+
+
+      
+     
+
+     
+
+
+
+
+
